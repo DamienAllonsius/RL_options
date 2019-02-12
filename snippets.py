13a4bd3f7428a51bcb474fb9c2e420735f350dea
dallonsi <damien.allonsius@univ-amu.fr>
2019-02-12 18:17:50 +0100

    added zone_size_x and zone_size_y

--- snippets.py
+++ snippets.py
@@ -6,22 +6,25 @@ from gym.envs.classic_control import rendering
 
 class ObservationZoneWrapper(gym.ObservationWrapper):
 
-    def __init__(self, env, downsampled_size_x, downsampled_size_y, blurred, number_gray_colors=0):
+    def __init__(self, env, zone_size_x, zone_size_y, blurred, number_gray_colors = 0):
 
         super(gym.ObservationWrapper, self).__init__(env)
         self.blurred = blurred
-        self.downsampled_size = (downsampled_size_x, downsampled_size_y)
+        self.zone_size_x = zone_size_x
+        self.zone_size_y = zone_size_y
         self.number_gray_colors = number_gray_colors
     
     def observation(self, observation):
-        img_blurred = cv2.resize(observation, self.downsampled_size, interpolation=cv2.INTER_AREA)
+        len_y = len(observation) # with MontezumaRevenge-v4 : 160
+        len_x = len(observation[0]) # with MontezumaRevenge-v4 : 210
+        img_blurred = cv2.resize(observation, (len_x // self.zone_size_x , len_y // self.zone_size_y), interpolation=cv2.INTER_AREA)
         img_blurred_resized = cv2.resize(img_blurred, (512, 512), interpolation=cv2.INTER_NEAREST)
         return img_blurred_resized
 
-env = ObservationZoneWrapper(gym.make("MontezumaRevenge-v4"),  downsampled_size_x = 70, downsampled_size_y = 70, blurred = True)
+env = ObservationZoneWrapper(gym.make("MontezumaRevenge-v4"),  zone_size_x = 20, zone_size_y = 20, blurred = True)
 obs = env.reset()
 
 viewer = rendering.SimpleImageViewer()
 viewer.imshow(obs)
 
-time.sleep(100)
+time.sleep(1)
