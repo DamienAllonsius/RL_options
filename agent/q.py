fd124b26e9ea98b4a421ebe20e15a6940a957d86
dallonsi <damien.allonsius@univ-amu.fr>
2019-02-13 11:56:19 +0100

    q function refactoring : states are images now !

--- agent/q.py
+++ agent/q.py
@@ -4,83 +4,72 @@ from variables import *
 class QAbstract(object):
     """ 
     Contains the q value function which maps a state and an action to a value
-    q_dict is a dictionary q_dict = {state_* : {action_* : value}}
-    TODO : replace self.q_dict by a DictUpdate object
+    q is a list of dictionaries q[n] = {action_1 : value_1, action_2 : value_2}
+    TODO : find a way to print the states. Maybe print the screen ? 
     """
     def __init__(self, state):
-        self.q_dict = {state : {}}
-
+        self.state_list = [state]
+        self.q = [{}]
+        self.number_states = 1
+        
     def __str__(self):
         message = ""
-        for state in self.q_dict:
-            for action in self.q_dict[state]:
-                message += "state " +str(state) + " action " + str(action) + " value : " + str(self.q_dict[state][action]) + "\n"
+        for state_index in self.q:
+            for action in self.q[state_index]:
+                message += "state " +str(self.state_list[state_index]) + " action " + str(action) + " value : " + str(self.q_dict[state_index][action]) + "\n"
                 
         return message
 
+    def is_state(self, state):
+        return state in self.state_list
+
+    def is_actions(self, state):
+        if not(self.is_state(state)):
+            raise Exception("cannot test if actions exist since state does not exist")
+        
+        else:
+            return self.q[self.state_list.index(state)] != {}
+
+    def is_action_to_state(self, state, action):
+        if self.is_state(state) and self.is_actions(state):
+            return action in self.q_dict[self.state_list.index(state)]
+        
     def add_state(self, state):
         """
         If state does not exist, we create it.
         Otherwise, we do nothing.
-        returns True iif state already existed
         """
-        try:
-            self.q_dict[state]
-            
-        except:
-            self.q_dict.update({state : {}})
-
+        if not(self.is_state(state)):
+            self.state_list.append(state)
+            self.q.append({})
+            self.number_states += 1
+        
     def add_action_to_state(self, state, action, reward = 0):
         """
+        ? idea ? :
         does not add anything if 
         for action in q[option.terminal_state]:
            action.terminal_state = option.initial_state
         """
-        try:
-            self.q_dict[state]
-            
-        except:
+        if not(self.is_state(state)):
             raise Exception("action cannot be added since state does not exist")
-        
-        total_actions = self.q_dict[state]
-        if total_actions == {}:
-            self.q_dict[state].update({action : reward})
-            
+
         else:
-            if action not in total_actions:
-                self.q_dict[state].update({action : reward})
+            self.q[self.state_list.index(state)].update({action : reward})
         
     def find_best_action(self, state):
-        try:
-            self.q_dict[state]
-            
-        except:
+        if not(self.is_state(state)):
             raise Exception('cannot find best action since there is no state : ' + str(state))
         
-        if not(self.is_actions(state)):
+        elif not(self.is_actions(state)):
             raise Exception('cannot find best action since there is no action in state ' + str(state))
-        
-        best_action = None
-        best_reward = - float('inf')
-        total_actions = self.q_dict[state]
-        
-        for action in total_actions:
-            reward = total_actions[action]
-            if reward > best_reward:
-                best_reward = reward
-                best_action = action
-                
-        return best_reward, best_action
 
-    def is_actions(self, state):
-        return self.q_dict[state] != {}
-
-    def is_action_to_state(self, state, action):
-        return action in self.q_dict[state]
-    
-    def is_state(self, state):
-        return state in self.q_dict
-
+        else: # return best_value, best_action
+            idx = self.state_list.index(state)
+            values = list(self.q[idx].values())
+            actions = list(self.q[idx].keys())
+            return max(values), actions[values.index(max(values))]    
+ 
     def update_q_dict_action_space(self, state, new_state, action, reward):
         self.add_action_to_state(state, action, reward)
         self.add_state(new_state)
@@ -97,18 +86,17 @@ class Q(QAbstract):
         (1- learning_rate) * Q_t(current_position, action)
         + learning_rate * [reward + max_{actions} Q_(new_position, action)]
         """
-        try:
-            self.q_dict[state]
+        if self.is_state(state):
+            if self.is_actions(new_state):
+                best_value, _ = self.find_best_action(new_state)
 
-        except:
-            raise Exception('state cannot be updated since it does not exist')
+            else:
+                best_value = 0
 
-        if self.q_dict[new_state] == {}:
-            max_value_action = 0
-            
+            idx = self.state_list.index(state)
+            self.q[idx][action] *= (1 - LEARNING_RATE)
+            self.q[idx][action] += LEARNING_RATE * (reward + best_value)
+
         else:
-            max_value_action, _ = self.find_best_action(new_state)
-            
-        self.q_dict[state][action] *= (1 - LEARNING_RATE)
-        self.q_dict[state][action] += LEARNING_RATE * (reward + max_value_action)
-        
+            raise Exception('unhable to update q since state does not exist')
+
