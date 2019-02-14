894475fb6116ce70db8c0af7b6ea2041c88de843
dallonsi <damien.allonsius@univ-amu.fr>
2019-02-14 17:38:21 +0100

    finally could resize the atari image

--- snippets.py
+++ snippets.py
@@ -7,7 +7,7 @@ from gym.envs.classic_control import rendering
 from variables import *
 sys.path.append('gridenvs')
 import gridenvs.examples  # load example gridworld environments
-
+from gym.envs.classic_control import rendering
 
 class ObservationZoneWrapper(gym.ObservationWrapper):
     """
@@ -19,6 +19,22 @@ class ObservationZoneWrapper(gym.ObservationWrapper):
         self.zone_size_y = zone_size_y
         self.blurred = blurred
         
+    def render(self, size = (512, 512), mode = 'human', close = False):
+        if hasattr(self.env.__class__, 'render_scaled'): # we call render_scaled function from gridenvs
+            return self.env.render_scaled(size, mode, close)
+         
+        else: # we scale the image from other environment (like Atari)
+            img = self.env.env.ale.getScreenRGB2()
+            img = cv2.resize(img, size, interpolation=cv2.INTER_NEAREST)
+            if mode == 'rgb_array':
+                return img
+            elif mode == 'human':
+                from gym.envs.classic_control import rendering
+                if self.env.env.viewer is None:
+                    self.env.env.viewer = rendering.SimpleImageViewer()
+                    self.env.env.viewer.imshow(img)
+                return self.env.env.viewer.isopen
+                 
     def observation(self, observation):
         if self.blurred:
             len_y = len(observation) # with MontezumaRevenge-v4 : 160
@@ -26,10 +42,13 @@ class ObservationZoneWrapper(gym.ObservationWrapper):
             if (len_x % self.zone_size_x == 0) and (len_y % self.zone_size_y == 0):
                 downsampled_size = (len_x // self.zone_size_x , len_y // self.zone_size_y)
                 img_blurred = cv2.resize(observation, downsampled_size, interpolation=cv2.INTER_AREA) # vector of size "downsampled_size"
+                # transform the observation in tuple
+                #tuple(map(tuple,img_blurred)))
+                #time.sleep(10)
                 return img_blurred
             
             else:
-                raise Exception("The gridworld can not be fragmented into zones")
+                raise Exception("The gridworld " + str(len_x) + "x" + str(len_y) +  " can not be fragmented into zones " + str(self.zone_size_x) + "x" + str(self.zone_size_y))
         
         else:
             return observation
